623593ea80fcd5a6969e23fd27020bb17e242b5e
ManivannanE <manivannanelangovan@syncfusion.com>
2018-10-11 16:59:27 +0530

    CustomFont sample committed for all platforms.

---
+++ CustomFont/CustomFont/CustomFont/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CustomFont
+{
+    public class ViewModel
+    {
+		public ObservableCollection<Model> GoldData { get; set; }
+
+		public ObservableCollection<Model> SilverData { get; set; }
+
+		public ObservableCollection<Model> BronzeData { get; set; }
+
+		public ViewModel()
+		{
+			GoldData = new ObservableCollection<Model>
+			{
+				new Model("USA", 46),
+				new Model("GBR", 27),
+				new Model("CHN", 26),
+			};
+
+			SilverData = new ObservableCollection<Model>
+			{
+				new Model("USA", 37),
+				new Model("GBR", 23),
+				new Model("CHN", 18),
+			};
+
+			BronzeData = new ObservableCollection<Model>
+			{
+				new Model("USA", 38),
+				new Model("GBR", 17),
+				new Model("CHN", 26),
+			};
+		}
+    }
+}
